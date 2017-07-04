4f93bffe701cc6cff906bdb717a0b47eda4252f2
Li Liu <finley@Lis-MacBook-Pro.local>
2017-07-04 22:30:58 +0800

    jenkins测试

--- src/main/java/com/changyi/fi/component/merchant/MerchantResource.java
+++ src/main/java/com/changyi/fi/component/merchant/MerchantResource.java
@@ -1,10 +1,30 @@
 package com.changyi.fi.component.merchant;
 
+import com.changyi.fi.component.merchant.response.MerchantValidateResponse;
+import com.changyi.fi.component.merchant.service.MerchantService;
+import com.changyi.fi.core.annotation.Secured;
+import com.changyi.fi.core.response.Response;
+import com.changyi.fi.core.token.Token;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.ws.rs.Path;
+import javax.annotation.Resource;
+import javax.ws.rs.*;
 
 @Component
 @Path("merchant")
 public class MerchantResource {
+
+    @Resource
+    private MerchantService merchantService;
+
+    @POST
+    @Path("/validate")
+    @Consumes("application/json")
+    @Produces("application/json")
+    @Secured
+    public Response validate(@HeaderParam(Token.KEY) String token, @RequestParam String request) {
+        return new MerchantValidateResponse();
+    }
+
 }
---
+++ src/main/java/com/changyi/fi/component/merchant/response/MerchantValidateResponse.java
@@ -0,0 +1,6 @@
+package com.changyi.fi.component.merchant.response;
+
+import com.changyi.fi.core.response.NormalResponse;
+
+public class MerchantValidateResponse extends NormalResponse  {
+}
