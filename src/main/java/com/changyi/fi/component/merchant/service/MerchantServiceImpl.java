8b066ee386a16805dd5adb148ae45bfbeaec5ffe
Li Liu <finley@lis-macbook-pro.cn.ibm.com>
2017-07-05 10:15:19 +0800

    增加merchant service的实现

---
+++ src/main/java/com/changyi/fi/component/merchant/service/MerchantServiceImpl.java
@@ -0,0 +1,7 @@
+package com.changyi.fi.component.merchant.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("merchantService")
+public class MerchantServiceImpl implements MerchantService {
+}
