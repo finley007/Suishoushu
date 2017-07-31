e146c92daf6c34d7ba1eea60168b050c8f59ab50
Li Liu <finley@Lis-MacBook-Pro.local>
2017-07-31 23:12:22 +0800

    登录后保存customer信息

--- src/main/java/com/changyi/fi/auth/AuthServiceImpl.java
+++ src/main/java/com/changyi/fi/auth/AuthServiceImpl.java
@@ -1,8 +1,10 @@
 package com.changyi.fi.auth;
 
+import com.changyi.fi.component.customer.service.CustomerService;
 import com.changyi.fi.core.LogUtil;
 import com.changyi.fi.core.token.Token;
 import com.changyi.fi.external.weixin.WeixinAPIService;
+import com.changyi.fi.vo.Customer;
 import com.changyi.fi.vo.Session;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +21,15 @@ public class AuthServiceImpl implements AuthService {
     @Resource
     private WeixinAPIService weixinAPIService;
 
+    @Resource
+    private CustomerService customerService;
+
     public AuthResponse authenticate(String code) throws Exception {
         LogUtil.info(this.getClass(), "Execute authenticate service for code: " + code);
         //调用微信服务获取当前登录用户信息
         Session session = weixinAPIService.login(code);
         //根据openid生成session并保存在token中
+        customerService.updateCustomer(new Customer(), session.getOpenId());
         return new AuthResponse(new Token(session).getKey());
     }
 
@@ -31,6 +37,7 @@ public class AuthServiceImpl implements AuthService {
         LogUtil.info(this.getClass(), "Execute internalAuthenticate service for code: " + code);
         Session session = new Session();
         session.setOpenId(TESTER_CODE);
+        customerService.updateCustomer(new Customer(), session.getOpenId());
         return new AuthResponse(new Token(session).getKey());
     }
 
