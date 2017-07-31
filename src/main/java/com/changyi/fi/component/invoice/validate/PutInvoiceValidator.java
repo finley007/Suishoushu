750989098339b7e09d1c4db93a5b049406ce3a32
Li Liu <finley@Lis-MacBook-Pro.local>
2017-07-31 23:35:58 +0800

    企业发票地址电话开户行开户行账户都不是必填的

--- src/main/java/com/changyi/fi/component/invoice/validate/PutInvoiceValidator.java
+++ src/main/java/com/changyi/fi/component/invoice/validate/PutInvoiceValidator.java
@@ -15,15 +15,16 @@ public class PutInvoiceValidator implements Validator<PutInvoiceRequest> {
         if (FIConstants.InvoiceType.Enterprise.getValue().equals(request.getType())) {
             if (StringUtils.isEmpty(request.getCreditCode())) {
                 throw new InvalidRequestException("Field creditCode is required");
-            } else if (StringUtils.isEmpty(request.getAddress())) {
-                throw new InvalidRequestException("Field address is required");
-            } else if (StringUtils.isEmpty(request.getBank())) {
-                throw new InvalidRequestException("Field bank is required");
-            } else if (StringUtils.isEmpty(request.getBankAcct())) {
-                throw new InvalidRequestException("Field bankAcct is required");
-            } else if (StringUtils.isEmpty(request.getPhone())) {
-                throw new InvalidRequestException("Field phone is required");
             }
+//            else if (StringUtils.isEmpty(request.getAddress())) {
+//                throw new InvalidRequestException("Field address is required");
+//            } else if (StringUtils.isEmpty(request.getBank())) {
+//                throw new InvalidRequestException("Field bank is required");
+//            } else if (StringUtils.isEmpty(request.getBankAcct())) {
+//                throw new InvalidRequestException("Field bankAcct is required");
+//            } else if (StringUtils.isEmpty(request.getPhone())) {
+//                throw new InvalidRequestException("Field phone is required");
+//            }
         }
     }
 }
