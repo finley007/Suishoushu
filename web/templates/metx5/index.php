141544d5e182f646db9621aa1e637e9692635952
Li Liu <finley@Lis-MacBook-Pro.local>
2017-10-06 15:01:48 +0800

    调整首页板块位置

--- web/templates/metx5/index.php
+++ web/templates/metx5/index.php
@@ -5,10 +5,6 @@ echo <<<EOT
 <!--
 EOT;
 $w = 0;
-if($lang_about_open){
-$w++;
-require_once template('index/about');
-}
 if($lang_product_open){
 $w++;
 $into = $w%2==0?'tem_index_to':'';
@@ -23,6 +19,10 @@ if($lang_case_open){
 $w++;
 $into = $w%2==0?'tem_index_to':'';
 require_once template('index/case');
+}
+if($lang_about_open){
+$w++;
+require_once template('index/about');
 }
 if($lang_footer_open){
 require_once template('index/footer');
