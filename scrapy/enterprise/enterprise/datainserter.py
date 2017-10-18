25c393171a21291174324568eff0bf3fefd58ea1
Li Liu <finley@Lis-MacBook-Pro.local>
2017-10-18 22:21:28 +0800

    导入爬虫数据

--- scrapy/enterprise/enterprise/datainserter.py
+++ scrapy/enterprise/enterprise/datainserter.py
@@ -48,10 +48,10 @@ def main(argv):
 def solveFile(file, dbclient):
 	logging.info("************Solve file: " + file)
 	for line in open(file):
-		logging.info(line)
+		#logging.info(line)
 		data = line.split("|")
-		if len(data) <= 1 or data[3] == '未公开':
-			logging.info("************Invalid record:" + line)
+		if len(data) <= 1 or data[3] == '未公开' or data[3].strip() == '':
+			#logging.info("************Invalid record:" + line)
 			continue
 		regCapital = getDigital(data[6])
 		bizPeriodStart = ""
@@ -65,14 +65,15 @@ def solveFile(file, dbclient):
 		if len(findAll(r'\d{4}-\d{2}-\d{2}', data[7])) > 1:
 			 establishDate = data[7]
 		insertSQL = createInsertSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDate)
+		#print data[3]
 		logging.info("Insert SQL: " + insertSQL)
 		result = dbclient.updateDB(insertSQL)
 		if result == -1:
 			updateSQL =  createUpdateSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDate)
-			logging.info("Update SQL: " + updateSQL)
+			#logging.info("Update SQL: " + updateSQL)
 			result = dbclient.updateDB(updateSQL)
-			if result == -1:
-				logging.info("************Error for enterprise " + data[3] + "|" + data[4])
+			#if result == -1:
+				#logging.info("************Error for enterprise " + data[3] + "|" + data[4])
 
 def getDigital(text):
 	result = re.match(r'[\d.]*', text)
@@ -87,7 +88,7 @@ def findAll(regex, text):
 
 def createInsertSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDate):
 	insert = "insert into inc_enterprise (credit_code,name,legal_person,reg_capital,reg_authority,address,phone,biz_reg_num,org_code,taxpayer_code,industry,area, create_by"
-	values = " values ('" + data[3] + "','" + data[4] + "','" + data[5] + "','" + str(regCapital) + "','" + data[9] + "','" + data[10] + "','" + data[11] + "','" + data[12] + "','" + data[13] + "','" + data[14] + "','" + data[15].replace("\n", "") + "','" + data[0] + "','system'"
+	values = " values ('" + data[3] + "','" + data[4] + "','" + filterVal(data[5]) + "','" + str(filterVal(regCapital)) + "','" + filterVal(data[9]) + "','" + filterVal(data[10]) + "','" + filterVal(data[11]) + "','" + filterVal(data[12]) + "','" + filterVal(data[13]) + "','" + filterVal(data[14]) + "','" + data[15].replace("\n", "") + "','" + data[0] + "','system'"
 	if bizPeriodStart != '':
 		insert = insert + ",biz_period_start"
 		values = values + ",'" + bizPeriodStart + "'"
@@ -102,7 +103,7 @@ def createInsertSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDat
 	return insert + values
 
 def createUpdateSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDate):
-	update = "update inc_enterprise t set t.legal_person = '" + data[5] + "', t.reg_capital = '" + str(regCapital) + "', t.reg_authority = '" + data[9] + "', t.address = '" + data[10] + "', t.phone = '" + data[11] + "', t.biz_reg_num = '" + data[12] + "', t.org_code = '" + data[13] + "', t.taxpayer_code = '" + data[14] + "', t.industry = '" + data[15] + "', t.area = '" + data[0]
+	update = "update inc_enterprise t set t.legal_person = '" + filterVal(data[5]) + "', t.reg_capital = '" + str(filterVal(regCapital)) + "', t.reg_authority = '" + filterVal(data[9]) + "', t.address = '" + filterVal(data[10]) + "', t.phone = '" + filterVal(data[11]) + "', t.biz_reg_num = '" + filterVal(data[12]) + "', t.org_code = '" + filterVal(data[13]) + "', t.taxpayer_code = '" + filterVal(data[14]) + "', t.industry = '" + data[15] + "', t.area = '" + data[0]
 	if bizPeriodStart != '':
 		update = update + "', t.biz_period_start = '" + bizPeriodStart
 	if bizPeriodEnd != '':
@@ -111,6 +112,12 @@ def createUpdateSql(data, bizPeriodStart, bizPeriodEnd, regCapital, establishDat
 		update = update + "', t.establish_date = '" + establishDate
 	update = update + "' where t.credit_code = '" + data[3] + "'"
 	return update
+
+def filterVal(value):
+	if value == '暂无' or value == '未公开':
+		return ""
+	else:
+		return value
 
 if __name__ == "__main__":
    main(sys.argv[1:])
