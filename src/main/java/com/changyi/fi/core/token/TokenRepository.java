76ef1f845636252775c39a0f2dbe1d9acd31adfa
Li Liu <finley@Lis-MacBook-Pro.local>
2017-10-21 11:03:00 +0800

    微信号重新登录之后清理不需要的token实例

--- src/main/java/com/changyi/fi/core/token/TokenRepository.java
+++ src/main/java/com/changyi/fi/core/token/TokenRepository.java
@@ -5,7 +5,9 @@ import com.changyi.fi.core.LogUtil;
 import com.changyi.fi.core.Payload;
 import com.changyi.fi.core.redis.RedisClient;
 import com.changyi.fi.vo.Session;
+import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.List;
@@ -14,6 +16,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The type Token repository.
+ */
 public class TokenRepository implements ITokenRepository{
 
     private static final String SESSION_LIST_KEY = "sessionList";
@@ -24,11 +29,10 @@ public class TokenRepository implements ITokenRepository{
     public void addToken(Token token) {
         tokens.put(token.getKey(), token);
         tokenRemoverPool.schedule(new TokenRemover(token.getKey()), token.getDelay(), TimeUnit.MILLISECONDS);
-        traceTokens();
+        removeUnusedTokens(token.getOpenId());
     }
 
     public Token touchToken(String key) {
-        traceTokens();
         return tokens.get(key);
     }
 
@@ -36,17 +40,30 @@ public class TokenRepository implements ITokenRepository{
         tokens.remove(key);
     }
 
-    private void traceTokens() {
-        Enumeration<String> en = tokens.keys();
-        while (en.hasMoreElements()) {
-            System.out.println(en.nextElement());
+
+    private void removeUnusedTokens(String openId) {
+        List<String> unusedKeys = new ArrayList<String>();
+        if (StringUtils.isNotBlank(openId)) {
+            Enumeration<String> keys = tokens.keys();
+            while (keys.hasMoreElements()) {
+                String key = keys.nextElement();
+                if (openId.equals(tokens.get(key).getOpenId())) {
+                    unusedKeys.add(key);
+                }
+            }
+        }
+        if (unusedKeys != null && unusedKeys.size() > 0) {
+            LogUtil.debug(this.getClass(), "Remove unused tokens: ");
+            for (String key : unusedKeys) {
+                LogUtil.debug(this.getClass(), "key|" + key);
+                removeToken(key);
+            }
         }
     }
 
     public void updateToken(Token token) {}
 
     public void saveToken() {
-        traceTokens();
         RedisClient.del(SESSION_LIST_KEY);
         for (String key : tokens.keySet()) {
             Token token = tokens.get(key);
@@ -74,6 +91,11 @@ public class TokenRepository implements ITokenRepository{
     private class TokenRemover implements Runnable {
         private String tokenKey;
 
+        /**
+         * Instantiates a new Token remover.
+         *
+         * @param tokenKey the token key
+         */
         public TokenRemover(String tokenKey) {
             this.tokenKey = tokenKey;
         }
@@ -96,6 +118,12 @@ public class TokenRepository implements ITokenRepository{
 
     private class SessionStorage extends BaseVO {
 
+        /**
+         * Instantiates a new Session storage.
+         *
+         * @param key     the key
+         * @param session the session
+         */
         public SessionStorage(String key, Session session) {
             this.key = key;
             this.openId = session.getOpenId();
@@ -106,6 +134,11 @@ public class TokenRepository implements ITokenRepository{
         private String openId;
         private String sessionKey;
 
+        /**
+         * Gets key.
+         *
+         * @return the key
+         */
         public String getKey() {
             return key;
         }
