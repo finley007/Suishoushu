c07362d630f5488392ff7346e088e61e7875b780
Li Liu <finley@Lis-MacBook-Pro.local>
2018-01-07 11:42:47 +0800

    增加商户接口

--- src/main/java/com/changyi/fi/component/endpoint/EndpointResource.java
+++ src/main/java/com/changyi/fi/component/endpoint/EndpointResource.java
@@ -24,12 +24,31 @@ public class EndpointResource {
         LogUtil.info(this.getClass(), "Enter listEndpoints endpoint");
         try {
             String response = FileReader.readFileContentByClasspath("endpoints.json");
-            LogUtil.info(this.getClass(), "Complete matchEnterprise endpoint handle");
+            LogUtil.info(this.getClass(), "Complete listEndpoints endpoint handle");
             LogUtil.debug(this.getClass(), "Response: {} ", response);
             return Response.status(Response.Status.OK).
                     header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON + ";charset=UTF-8").entity(response).build();
         } catch (Throwable t) {
-            LogUtil.error(this.getClass(), "Run matchEnterprise endpoint error: ", t);
+            LogUtil.error(this.getClass(), "Run listEndpoints endpoint error: ", t);
+            String res = ExceptionHandler.handle(t);
+            return Response.status(Response.Status.OK).entity(res).build();
+        }
+    }
+
+    @GET
+    @Path("merchant")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Timer
+    public Response listMerchantEndpoints() {
+        LogUtil.info(this.getClass(), "Enter listMerchantEndpoints endpoint");
+        try {
+            String response = FileReader.readFileContentByClasspath("merchant-endpoints.json");
+            LogUtil.info(this.getClass(), "Complete listMerchantEndpoints endpoint handle");
+            LogUtil.debug(this.getClass(), "Response: {} ", response);
+            return Response.status(Response.Status.OK).
+                    header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON + ";charset=UTF-8").entity(response).build();
+        } catch (Throwable t) {
+            LogUtil.error(this.getClass(), "Run listMerchantEndpoints endpoint error: ", t);
             String res = ExceptionHandler.handle(t);
             return Response.status(Response.Status.OK).entity(res).build();
         }
